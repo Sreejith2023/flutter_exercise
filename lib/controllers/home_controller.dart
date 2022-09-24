59306b9836283717ba89d3c21899e9438b063952
Sreejith Surendran <sreejith2023@gmail.com>
2022-09-24 17:46:06 +0530

    search logic integrated

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -7,27 +7,49 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController{
   RxList<Rows> homeList = List.filled(0,Rows(),growable: true).obs;
+  RxList<Rows> filteredList = List.filled(0,Rows(),growable: true).obs;
+  RxString title = "".obs;
+  RxBool isLoading = false.obs;
   @override
   void onInit() {
-    getHomeData();
+    getHomeData(true);
     super.onInit();
   }
 
-  getHomeData() async{
+  getHomeData(bool updateLoading) async{
+    if(updateLoading) isLoading.value = true;
     var response = await NetworkHandler()
         .get(AppApis.apiUrl);
 
     RemoteResponseModel remoteResponseModel = RemoteResponseModel.fromJson(jsonDecode(response));
+    if(updateLoading) isLoading.value = false;
     print("response ${remoteResponseModel.toString()}");
+    title.value = remoteResponseModel.title!;
     setHomeListData(remoteResponseModel.rows!);
 
   }
 
   setHomeListData(List<Rows> data){
     homeList.value = data;
+    filteredList.value.clear();
+    filterResponse("");
+
   }
 
   List<Rows> getHomeListData(){
     return homeList.value;
+  }
+
+  List<Rows> getFilteredListData(){
+    return filteredList.value;
+  }
+
+  filterResponse(String key){
+    for(Rows element in homeList){
+      if(element.title!.toLowerCase().contains(key.toLowerCase())){
+        filteredList.value.add(element);
+        filteredList.refresh();
+      }
+    }
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,23 +5,25 @@ import 'package:flutter_exercise/controllers/home_controller.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends GetView<HomeController> {
+  final searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _appBar(),
-      body: RefreshIndicator(
-        onRefresh: _refreshPage,
-        child: listWidget(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: _appBar(),
+        body: RefreshIndicator(
+          onRefresh: _refreshPage,
+          child: listWidget(),
+        ),
       ),
     );
   }
 
   listWidget() {
     return Obx(() {
-      return ListView.builder(
-          itemCount: controller.homeList.length,
+      return controller.isLoading.value? progressWidget():controller.getFilteredListData().length>0?ListView.builder(
+          itemCount: controller.getFilteredListData().length,
           itemBuilder: (BuildContext context, int index) {
-            print("current index $index");
             return Card(
               margin: EdgeInsets.all(8),
               child: Padding(
@@ -32,15 +34,15 @@ class HomeScreen extends GetView<HomeController> {
                       alignment: Alignment.centerLeft,
                       margin: EdgeInsets.symmetric(vertical: 5),
                       child:
-                          Text("Title : ${controller.homeList[index].title!}"),
+                          Text("Title : ${controller.getFilteredListData()[index].title!}"),
                     ),
                     Container(
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.symmetric(vertical: 5),
                         child: Text(
-                            "Description : ${controller.homeList[index].description!}")),
+                            "Description : ${controller.getFilteredListData()[index].description!}")),
                     CachedNetworkImage(
-                      imageUrl: controller.homeList[index].imageHref!,
+                      imageUrl: controller.getFilteredListData()[index].imageHref!,
                       // imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg',
                       placeholder: (context, url) => Image(
                           image: AssetImage('asset/images/ic_profile.png')),
@@ -51,20 +53,60 @@ class HomeScreen extends GetView<HomeController> {
                 ),
               ),
             );
-          });
+          }):noDataWidget();
     });
   }
 
+  Widget progressWidget(){
+    return const Center(child: CircularProgressIndicator(),);
+  }
+
+  Widget noDataWidget(){
+    return const Center(child: Text("No Data"),);
+  }
+
   Future<void> _refreshPage() async {
-    controller.getHomeData();
+    controller.getHomeData(false);
   }
 
   AppBar _appBar() {
     return AppBar(
       centerTitle: true,
-      title: Text('App Title'),
-      flexibleSpace: Container(
-        child: TextField(),
+      toolbarHeight: 100,
+      flexibleSpace: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              child: Obx((){
+                return Text(
+                  controller.title.value,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                );
+              })),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.blueAccent),
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search Title Here...',
+              ),
+              onChanged: (value){
+                controller.filteredList.value.clear();
+                controller.filteredList.refresh();
+                controller.filterResponse(value);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
